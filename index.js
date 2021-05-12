64d4541860beff63b728304d97a25f9fb2df3bb5
HS230-Arch <ludovit.34@gmail.com>
2021-05-12 12:49:52 +0200

    restructured to functions and minor fixes

--- index.js
+++ index.js
@@ -7,27 +7,35 @@ const io = new Server(server);
 
 app.use(express.static('public')); // Serve Static Assets
 
-//Variables for the app and game start here, above are essential things for server operation.
+//Variables for the app and game start here, above are more environmental stuff
 var connectionCounter = 0; //Monitor current connections
-var connectionIDs = [];
-var joinedPlayers = {};
-var readyPlayers = {};
+var connectionIDs = []; //array of all current connections
+var joinedPlayers = {};  //players that have joined with a name, key is  sID, with value player name
+var readyPlayers = {}; //players that have joined and are ready to start, same format as above
 //game related variables:
 var gameRunning = false;
 const words = [["Stuhl", "Sofa"], ["Computer", "Notebook"], ["Wassereis", "Lolli"], ["Schreibstift", "Kugelschreiber"], ["Cannabis", "Tabak"]];
-var game = {
+let game = {
   "players": [], //array with ids of players
   "pnames": {}, //copy of readyplayers when startting game
   "imposter": "", //id of the imposter
   "round": 1, //round counter, when = player count game ends.
   "playersOrder": [], //players that stil haven't said anything.
+  "usedWordSets": [], //keys of sets used
+  "setIndex": 0, //current set
+  "imposterIndex": 0, // if the imposter gets the first or the second word of the word pair
+  "canVote": false, //when a client sends a vote, it will only be registered if this is true
   "r1": { //each round is stored like this
     "ID1": "ID3", //id from players array, id1 voted id3
     "ID2": "ID1",
     "ID3": "imposter", //id3 was imposter, couldn't vote.
   },
+  "r2": { //sample round 2
+    "ID1": "imposter",
+    "ID2": "ID3",
+    "ID3": "ID1",
+  },
 };
-var canVote = false;
 
 io.on('connection', (socket) => {
   const sID = socket.id //so that it remembers when disconnecting who is disconnecting
@@ -64,8 +72,7 @@ io.on('connection', (socket) => {
         var readyCount = Object.keys(readyPlayers).length;
         var joinedCount = Object.keys(joinedPlayers).length;
         console.log(readyCount + "/" + joinedCount);
-
-        if (readyCount >= 3 && readyCount / joinedCount == 1) {
+        if (readyCount >= 3 && readyCount / joinedCount == 1) { //if more then three people are connected and all of them are ready, start the game
           startGame();
         }
       } else {
@@ -98,13 +105,18 @@ io.on('connection', (socket) => {
 
 
 //THE GAME ITSELF:
-function startGame() {
+
+function gameInit() { //initialize the game
   game = {
     "players": [], //array with ids of players
     "pnames": {}, //copy of readyplayers when startting game
     "imposter": "", //id of the imposter
     "round": 1, //round counter, when = player count game ends.
     "playersOrder": [], //players that stil haven't said anything.
+    "usedWordSets": [], //keys of sets used
+    "setIndex": 0, //current set
+    "imposterIndex": 0, // if the imposter gets the first or the second word of the word pair
+    "canVote": false,
   };
   gameRunning = true;
   console.log('start game says hi!')
@@ -117,85 +129,114 @@ function startGame() {
   io.sockets.emit("joinPlayersNames", joinedPlayers); //send empty joinedPlayers to clients
   io.sockets.emit("readyPlayers", readyPlayers); //send empty readyPlayers to the clients
   console.log(game)
-
   game["players"].forEach(e => { //send each player command to start game
     io.to(e).emit("startGame", "game start"); //TODO: the client doesn't do anything yet.
   });
+};
 
-  var usedWordSets = []; //keys of sets used
+function gameDecidePlayersOrder() {
+  game["playersOrder"] = [...game["players"]]; //make playersOrder a copy of players
+  for (i = game["playersOrder"].length; i > 0; i--) { //shuffle order of sIDs in game.playersOrder, this will determine who goes first
+    let index = Math.floor(Math.random() * game["playersOrder"].length); //random id with which to swap
+    let temp = game["playersOrder"][index];
+    game["playersOrder"][index] = game["playersOrder"][i - 1];
+    game["playersOrder"][i - 1] = temp;
+  };
+};
 
-  while (gameRunning) {
-    //game loop
-
-    //DECIDE PLAYERS ORDER
-    game["playersOrder"] = [...game["players"]]; //make playersOrder a copy of players
-    for (i = game["playersOrder"].length; i > 0; i--) { //shuffle order of sIDs in game.playersOrder, this will determine who goes first
-      let index = Math.floor(Math.random() * game["playersOrder"].length); //random id with which to swap
-      let temp = game["playersOrder"][index];
-      game["playersOrder"][index] = game["playersOrder"][i - 1];
-      game["playersOrder"][i - 1] = temp;
+function gameChoosoImposter() {
+  rndmax = 0 //the player with maximun random value will be the imposter.
+  for (key in game["players"]) { // for every player
+    num = Math.random();
+    if (num > rndmax) {
+      rndmax = num
+      game["imposter"] = game["players"][key]; //is imposter if has the largest number
     };
+  }
+};
 
-    //CHOOSE IMPOSTER
-    rndmax = 0 //the player with maximun random value will be the imposter.
-    for (key in game["players"]) { // for every player
-      num = Math.random();
-      if (num > rndmax) {
-        rndmax = num
-        game["imposter"] = game["players"][key]; //is imposter if has the largest number
-      };
-    }
-
-    //CHOOSE WORD SET
-    if (words.length == usedWordSets.length) { //if all word sets have already been used, this is an edge case
-      usedWordSets = [];
-    }
-    var impIndex = 0;
-    impIndex = Math.round(Math.random()); //the imposter gets word 0 or 1 of the array?
-    let haventFoundSet = true; //this is to avoid repetition
-    var questionIndex = 0;
-    while (haventFoundSet) {
-      questionIndex = Math.floor(Math.random() * words.length); //which set of words is used?
-      if (usedWordSets.indexOf(questionIndex) == -1) {
-        haventFoundSet = false;
-      }
-    }
-    usedWordSets.push(questionIndex);
-
-    //SEND EACH PLAYER ITS CORRESPONDING WORD
-    game["playersOrder"].forEach(e => {
-      if (e == game["imposter"]) { // if imposter
-        io.sockets.emit("sMsg", `${game["pnames"][e]} is talking:`);
-        io.to(e).emit("sMsg", "you are imposter, this is your word: " + words[questionIndex][impIndex]);
-      } else { // if not imposter
-        io.sockets.emit("sMsg", `${game["pnames"][e]} is talking:`);
-        io.to(e).emit("sMsg", "you are normal, this is your word: " + words[questionIndex][Math.abs(impIndex - 1)]);
-      };
-      //TODO: wait 60 seconds (for people to talk)
-    });
-
-    //TODO: start voting screen (who was the imposter?)
-    io.sockets.emit("sMsg", `Voting time!`);
-    //TODO: wait 10 seconds (voting time)
-
-
-    //END OF ROUND
-    console.log("end of round:" + game["round"] + " , game state below:");
-    console.log(game);
-
-    if (game["round"] == game["players"].length) { //if the round is the same number as 
-      gameRunning = false; //game finished
-    } else {
-      game["round"] += 1; //move on to next round
+function gameChooseWordSet() {
+  if (words.length == game["usedWordSets"].length) { //if all word sets have already been used, this is an edge case
+    game["usedWordSets"] = [];
+  }
+  var impIndex = 0;
+  impIndex = Math.round(Math.random()); //the imposter gets word 0 or 1 of the array?
+  var haventFoundSet = true; //this is to avoid repetition
+  var questionIndex = 0;
+  while (haventFoundSet) {
+    questionIndex = Math.floor(Math.random() * words.length); //which set of words is used?
+    if (game["usedWordSets"].indexOf(questionIndex) == -1) {
+      haventFoundSet = false;
     }
   }
+  game["usedWordSets"].push(questionIndex);
+  game["setIndex"] = questionIndex;
+  game["imposterIndex"] = impIndex;
+}
 
+function gameVote() {
+  game["canVote"] = true;
+  game["players"].forEach(e => { //send each player command to vote
+    io.to(e).emit("sMsg", `Voting time!`);
+  });
+}
+
+function gameSendWord(client) { //client is the sID to whom the word is sent
+  if (client == game["imposter"]) { // if imposter
+    game["players"].forEach(e => { // to every player
+      io.to(e).emit("sMsg", `${game["pnames"][client]} is talking:`); //send who is talking
+    });
+    io.to(client).emit("sMsg", "you are imposter, this is your word: " + words[game["setIndex"]][game["imposterIndex"]]);
+  } else { // if not imposter
+    game["players"].forEach(e => { // to every player
+      io.to(e).emit("sMsg", `${game["pnames"][client]} is talking:`); //send who is talking
+    });
+    io.to(client).emit("sMsg", "you are normal, this is your word: " + words[game["setIndex"]][Math.abs(game["imposterIndex"] - 1)]);
+  };
+}
+
+function gameEndOfRound() {
+  console.log("end of round:" + game["round"] + " , game state below:");
+  console.log(game);
+  if (game["round"] == game["players"].length) { //if the round is the same number as number of players
+    gameEnds();
+  } else {
+    game["round"] += 1; //move on to next round
+  }
+}
+
+function gameEnds() {
+  gameRunning = false; //game finished
   console.log('game ended')
   game["players"].forEach(e => { //send each player command to end game
     io.to(e).emit("endGame", "game end"); //TODO: the client doesn't do anything yet.
   });
-};
+}
 
+function startGame() {
+  gameInit();
+  while (gameRunning) { //GAME LOOP
+    //DECIDE PLAYERS ORDER
+    gameDecidePlayersOrder();
+    //CHOOSE IMPOSTER
+    gameChoosoImposter();
+    //CHOOSE WORD SET
+    gameChooseWordSet();
+
+    //SEND EACH PLAYER ITS CORRESPONDING WORD
+    game["playersOrder"].forEach(e => { //e is the sID
+      gameSendWord(e);
+      //TODO: wait 60 seconds (for people to talk)
+    });
+
+    //START THE VOTING
+    gameVote();
+    //TODO: wait 10 seconds (voting time)
+
+    //END OF ROUND
+    gameEndOfRound();
+  }
+};
 
 server.listen(8282, () => {
   console.log('listening on *:8282');
